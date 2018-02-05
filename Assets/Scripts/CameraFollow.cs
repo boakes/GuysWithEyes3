1edfeee748a5b1f31f0ce3f2e67f5952fae88058
mnewtonten <mnewton@trinity.edu>
2018-02-04 19:35:30 -0600

    Flash Gordan Scene
    Camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+
+    private Transform target;
+    private float distanceX;
+    private float distanceY;
+
+	// Use this for initialization
+	void Start () {
+
+        target = GameObject.Find("flash").transform.transform;
+        
+    }
+    
+    void Update()
+    {
+
+        distanceY = transform.position.y - (target.position.y + 15);
+        transform.position = new Vector3(target.position.x + 40, transform.position.y - distanceY/1000, target.position.z + 7);
+
+    }
+}
