6257dd53571b3b88b218c550db0836740e584744
Brent Oakes <boakes1@trinity.edu>
2018-02-04 19:48:44 -0600

    fixed

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject target;
+	public Vector3 offsetFromTarget;
+	public bool followTarget = true;
+
+	// Use this for initialization
+	void Start () {
+		offsetFromTarget = target.transform.position - transform.position;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(followTarget){
+			transform.position = target.transform.position - offsetFromTarget;
+		}
+	}
+}
