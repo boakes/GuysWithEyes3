e8597724dae8211f91ccb36a81abf782e030ec2b
jburnett2 <jburnett@trinity.edu>
2018-02-06 23:19:37 -0600

    CHOOSE YOUR RACER

---
+++ Assets/Scripts/CameraSpin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSpin : MonoBehaviour {
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(target.transform.position, Vector3.up, -40 * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/RandomGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomGenerator : MonoBehaviour {
+
+    public int randRacer;
+    public int randCar;
+    public int randAcc;
+    
+    public GameObject racer;
+    public GameObject car;
+    public GameObject acc;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public void DeleteAll()
+    {
+        GameObject[] characters = GameObject.FindGameObjectsWithTag("character");
+        foreach (GameObject o in characters)
+        {
+            Destroy(o);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DeleteAll();
+
+            randRacer = Random.Range(1, 10);
+            randCar = Random.Range(1, 13);
+            randAcc = Random.Range(1, 9);
+
+            racer = Resources.Load("Prefabs/Racers/" + randRacer) as GameObject;
+            car = Resources.Load("Prefabs/Cars/" + randCar) as GameObject;
+            acc = Resources.Load("Prefabs/Accessories/" + randAcc) as GameObject;
+            
+            racer = Instantiate(racer) as GameObject;
+            car = Instantiate(car) as GameObject;
+            acc = Instantiate(acc) as GameObject;
+        }
+	}
+}
+
+
